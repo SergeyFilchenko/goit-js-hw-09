fa1935436ce6587a5730d5abd41f6ac84f12f9d6
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-04-07 22:19:18 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const bodyColor = document.querySelector("body")
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
 
+startBtn.addEventListener('click', getRandomHexColor)
+
+function getRandomHexColor (){
+    timerId = setInterval(()=>{
+        return bodyColor.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+    }, 1000)
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+stopBtn.addEventListener('click', ()=>{
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+})
